8831f880e712dec6f81e2d5b9c44e8bf18210233
The1Glitch <easytmashabela@gamil.com>
2024-04-24 17:44:51 -0700

    fixed

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 # Setting up my client config file
 include stdlib
 
-file_line { 'Turn off passwrd auth':
+file_line { 'Turn off passwd auth':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => ' 	 PasswordAuthentication no',
