d43b274d147074f1997c8fa1d67d26a57cd54a0d
The1Glitch <easytmashabela@gmail.com>
2024-08-18 21:15:08 +0000

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# fixes the 'phpp' extension to 'php' in WordPress files 'wp-settings.php'.
+# Fixes bad "phpp" extensions to "php" in "wp-settings.php"
 
-exec { 'fix-wordpress':
-  command => 'sed =i s/phpp/php/g /var/www/html/wp-settings.php',
+exec{'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/'wp-settings.php',
   path	  => '/usr/local/bin/:/bin/'
 }
